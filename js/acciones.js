74562391733c6e9c4bf361b6d9873aea40612163
JavierRMorales <48559318+JavierRMorales@users.noreply.github.com>
2019-05-02 07:22:10 -0500

    11

--- js/acciones.js
+++ js/acciones.js
@@ -1,14 +1,14 @@
 //JavaScript Document
 
-$(document).ready(function(e) {
-    document.addEventListener("deviceready",function(){
+$(document).ready(function(e){
+document.addEventListener("deviceready",function(){
 		
-	$('#beep').tap(function(){
-	navigator.notification.beep(1);
+$('#beep').tap(function(){
+navigator.notification.beep(1);
 }); //tap beep 
 
-    $('#vibrar').tap(function(){
-	navigator.notification.vibrate(1000);
+$('#vibrar').tap(function(){
+navigator.notification.vibrate(1000);
 }); //tap vibrar
 
 },false); //deviceready
